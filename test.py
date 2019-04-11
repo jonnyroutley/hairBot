55654303b3dba16a6fa13a08cbe314df3e3475b9
Jonathan Routley <jonathan.routley@cmrsurgical.com>
2019-04-11 06:51:16 +0100

    fixed adjectives file/removed bad characters

--- test.py
+++ test.py
@@ -1,10 +1,20 @@
+# coding=utf-8
 # test = input("Try this")
 # if test == 'test':
 #     print("True")
 
 adjectives_file = "adjectives.txt"
-adjectives = str(open(adjectives_file).read()).split(" ")
-print(adjectives)
-adjectives = [item for item in adjectives if item.isalpha()]
+adjectives = str(open(adjectives_file).read()).split('\n')
+comparatives = []
+superlatives = []
+counter1 = 1
+counter2 = 2
+for i in range(len(adjectives)):
+    if counter1 % 3 == 0:
+        superlatives.append(adjectives[i])
+    if counter2 % 3 == 0:
+        comparatives.append(adjectives[i])
+    counter1 += 1
+    counter2 += 1
 
-print(adjectives)
+print(comparatives, superlatives)
