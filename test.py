6043fb52cdfb186c99f370ab262ea3ddf472cf45
Jonathan Routley <jonathan.routley@cmrsurgical.com>
2019-04-11 06:12:34 +0100

    debugging

--- test.py
+++ test.py
@@ -3,6 +3,8 @@
 #     print("True")
 
 adjectives_file = "adjectives.txt"
-adjectives = str(open(adjectives_file).read()).split(u'Â\xa0')
+adjectives = str(open(adjectives_file).read()).split(" ")
+print(adjectives)
+adjectives = [item for item in adjectives if item.isalpha()]
 
 print(adjectives)
