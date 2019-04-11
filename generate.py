2769f46f043e10fd879c81907327b92b0b9fa14f
Jonathan Routley <jonathan.routley@cmrsurgical.com>
2019-04-11 12:58:04 +0100

    minor edits

--- generate.py
+++ generate.py
@@ -91,6 +91,5 @@ def main():
         if switch == 6:
             return(one_name+ "'s " + random.choice(other_product_names) + ". Side Effects may include: " + random.choice(comparatives).capitalize() +" " +random.choice(body_parts).capitalize())
         
-for i in range(20):
-    print(main())
+print(main())
     
